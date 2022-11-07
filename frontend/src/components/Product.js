1a02e161aba6321cf81eaa969b9081cc834cb20c
Christopher Hage <hage.chris@yahoo.com>
2022-11-07 17:43:53 -0400

    rating and product card added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
+import Rating from "./Rating";
 const Product = ({ product }) => {
   return (
     <Card>
@@ -11,9 +12,9 @@ const Product = ({ product }) => {
         <Link to={`/product/${product.link}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
-
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add product to cart</Button>
+        <Button variant="warning">Add product to cart</Button>
       </Card.Body>
     </Card>
   );
